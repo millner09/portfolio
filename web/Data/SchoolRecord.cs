a21fcf45a575e0c94ec33f1a9e7ea12aacae8384
Adam Millner <adammillner@microsoft.com>
2021-08-26 13:12:28 -0500

    Updated theme, added education, updated awards, updated experience datetime rendering, and updated about me.

---
+++ web/Data/SchoolRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web.Data
+{
+    public class SchoolRecord
+    {
+        public string Name { get; set; }
+        public string Degree { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
